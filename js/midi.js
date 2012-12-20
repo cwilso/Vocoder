12ad5973f19bed93b155503933bfea2e0e5ee1e4
Chris Wilson <cwilso@gmail.com>
2012-12-20 11:07:48 -0800

    Conversion to new MIDI shim.

--- js/midi.js
+++ js/midi.js
@@ -28,77 +28,75 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-function midiMessageReceived( msgs ) {
-  for (i=0; i<msgs.length; i++) {
-    var cmd = msgs[i].data[0] >> 4;
-    var channel = msgs[i].data[0] & 0xf;
-    var b = msgs[i].data[1];
-    var c = msgs[i].data[2];
+function midiMessageReceived( e ) {
+  var cmd = e.data[0] >> 4;
+  var channel = e.data[0] & 0xf;
+  var b = e.data[1];
+  var c = e.data[2];
 
-    if ( cmd==8 || ((cmd==9)&&(c==0)) ) { // with MIDI, note on with velocity zero is the same as note off
+  if ( cmd==8 || ((cmd==9)&&(c==0)) ) { // with MIDI, note on with velocity zero is the same as note off
   //    if (b == lastNote) {   // this keeps from shutting off if we're overlapping notes
           // we don't currently need note off
   //        lastNote = -1;
   //    }
-    } else if (cmd == 9) {  // note on message
-      if (channel == 0 ) { // Change oscillator detune.
-        var noteNumber = b - 60;
-        var detuneValue = noteNumber * 100;
-        var detunegroup = document.getElementById("detunegroup");
-        $( detunegroup.children[1] ).slider( "value", detuneValue );
-        updateSlider( detunegroup, detuneValue, " cents" );
-        if (oscillatorNode)
-          oscillatorNode.detune.value = detuneValue;
-      } else if (channel == 1) { //pads - play previews
-        if (b==48)
-          previewModulator(); // is a toggle.
-        else if (b==49)
-          previewCarrier(); // is a toggle.
-        else if (b==44)
-          vocode(); // is a toggle.
-      }
-    } else if (cmd == 11) { // continuous controller
-      if (b == 1) {   // CC1: Modulator gain level
-        var value = Math.floor( (100 * c) / 63.5) / 50; // 0.0-4.0
-        var modgaingroup = document.getElementById("modgaingroup");
-        $( modgaingroup.children[1] ).slider( "value", value );
-        updateSlider( modgaingroup, value, "" );
-        modulatorGainValue = value;
-        if (modulatorGain)
-          modulatorGain.gain.value = value;
-      } else if (b == 2) {  // CC2: "Gender" - tuning frequencies
-        scaleCarrierFilterFrequencies((Math.floor( (100 * c) / 63.5) / 100) + 0.5) // ideally would be 0.5 - 2.0, centered on 1.
-      } else if (b == 5) {  //  CC5: Carrier sample level
-        var sampleValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
-        var samplegroup = document.getElementById("samplegroup");
-        $( samplegroup.children[1] ).slider( "value", sampleValue );
-        updateSlider( samplegroup, sampleValue, "" );
-        if (carrierSampleGain)
-          carrierSampleGain.gain.value = sampleValue;
-      } else if (b == 6) {  //  CC6: Carrier synth level
-        var synthValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
-        var synthgroup = document.getElementById("synthgroup");
-        $( synthgroup.children[1] ).slider( "value", synthValue );
-        updateSlider( synthgroup, synthValue, "" );
-        if (oscillatorGain)
-          oscillatorGain.gain.value = synthValue;
-      } else if (b == 7) {  //  CC7: Carrier noise level
-        var noiseValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
-        var noisegroup = document.getElementById("noisegroup");
-        $( noisegroup.children[1] ).slider( "value", noiseValue );
-        updateSlider( noisegroup, noiseValue, "" );
-        if (noiseGain)
-          noiseGain.gain.value = noiseValue;
-      } else if (b == 8) {  // CC8: HP filter gain
-        hpFilterGain.gain.value = c / 63.5; // 0.0-1.0
-      }
+  } else if (cmd == 9) {  // note on message
+    if (channel == 0 ) { // Change oscillator detune.
+      var noteNumber = b - 60;
+      var detuneValue = noteNumber * 100;
+      var detunegroup = document.getElementById("detunegroup");
+      $( detunegroup.children[1] ).slider( "value", detuneValue );
+      updateSlider( detunegroup, detuneValue, " cents" );
+      if (oscillatorNode)
+        oscillatorNode.detune.value = detuneValue;
+    } else if (channel == 1) { //pads - play previews
+      if (b==48)
+        previewModulator(); // is a toggle.
+      else if (b==49)
+        previewCarrier(); // is a toggle.
+      else if (b==44)
+        vocode(); // is a toggle.
+    }
+  } else if (cmd == 11) { // continuous controller
+    if (b == 1) {   // CC1: Modulator gain level
+      var value = Math.floor( (100 * c) / 63.5) / 50; // 0.0-4.0
+      var modgaingroup = document.getElementById("modgaingroup");
+      $( modgaingroup.children[1] ).slider( "value", value );
+      updateSlider( modgaingroup, value, "" );
+      modulatorGainValue = value;
+      if (modulatorGain)
+        modulatorGain.gain.value = value;
+    } else if (b == 2) {  // CC2: "Gender" - tuning frequencies
+      scaleCarrierFilterFrequencies((Math.floor( (100 * c) / 63.5) / 100) + 0.5) // ideally would be 0.5 - 2.0, centered on 1.
+    } else if (b == 5) {  //  CC5: Carrier sample level
+      var sampleValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
+      var samplegroup = document.getElementById("samplegroup");
+      $( samplegroup.children[1] ).slider( "value", sampleValue );
+      updateSlider( samplegroup, sampleValue, "" );
+      if (carrierSampleGain)
+        carrierSampleGain.gain.value = sampleValue;
+    } else if (b == 6) {  //  CC6: Carrier synth level
+      var synthValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
+      var synthgroup = document.getElementById("synthgroup");
+      $( synthgroup.children[1] ).slider( "value", synthValue );
+      updateSlider( synthgroup, synthValue, "" );
+      if (oscillatorGain)
+        oscillatorGain.gain.value = synthValue;
+    } else if (b == 7) {  //  CC7: Carrier noise level
+      var noiseValue = Math.floor( (100 * c) / 63.5) / 100; // 0.0-2.0
+      var noisegroup = document.getElementById("noisegroup");
+      $( noisegroup.children[1] ).slider( "value", noiseValue );
+      updateSlider( noisegroup, noiseValue, "" );
+      if (noiseGain)
+        noiseGain.gain.value = noiseValue;
+    } else if (b == 8) {  // CC8: HP filter gain
+      hpFilterGain.gain.value = c / 63.5; // 0.0-1.0
     }
   }
 }
 
 //init: create plugin
 window.addEventListener('load', function() {   
-  navigator.getMIDIAccess( gotMIDI, null );
+  navigator.requestMIDIAccess( gotMIDI, null );
 } );
 
 var midi = null;
